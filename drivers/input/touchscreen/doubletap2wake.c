7d547009461c46645e50fc4e9dc257f90cfce584
Loller79 <loller790@gmail.com>
2014-10-30 21:59:05 +0100

    DT2W and S2W Touchscreen Freezes Fix

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -58,9 +58,9 @@ MODULE_LICENSE("GPLv2");
 
 /* Tuneables */
 #define DT2W_DEBUG		0
-#define DT2W_DEFAULT		0
+#define DT2W_DEFAULT		1
 
-#define DT2W_PWRKEY_DUR		60
+#define DT2W_PWRKEY_DUR		0
 #define DT2W_FEATHER		90
 #define DT2W_TIME		700
 
