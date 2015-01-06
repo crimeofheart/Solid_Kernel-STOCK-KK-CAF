1b48bf9f8933033b09b7bec39f9e4cbc259d541f
Loller79 <loller790@gmail.com>
2015-01-06 14:47:25 +0100

    CPU Underclock

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -442,6 +442,7 @@ static struct acpu_level tbl_PVS6_2000MHz_factory_1026[] __initdata = {
 
 #endif //             
 static struct acpu_level tbl_slow[] __initdata = {
+	{ 1, {   270000, HFPLL, 2, 0x0C }, L2(0),   950000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   975000 },
 	{ 0, {   432000, HFPLL, 2, 0x20 }, L2(6),  1000000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),  1000000 },
@@ -470,6 +471,7 @@ static struct acpu_level tbl_slow[] __initdata = {
 };
 
 static struct acpu_level tbl_nom[] __initdata = {
+	{ 1, {   270000, HFPLL, 2, 0x0C }, L2(0),   900000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   925000 },
 	{ 0, {   432000, HFPLL, 2, 0x20 }, L2(6),   950000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),   950000 },
@@ -498,6 +500,7 @@ static struct acpu_level tbl_nom[] __initdata = {
 };
 
 static struct acpu_level tbl_fast[] __initdata = {
+	{ 1, {   270000, HFPLL, 2, 0x0C }, L2(0),   850000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   875000 },
 	{ 0, {   432000, HFPLL, 2, 0x20 }, L2(6),   900000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),   900000 },
@@ -526,6 +529,7 @@ static struct acpu_level tbl_fast[] __initdata = {
 };
 
 static struct acpu_level tbl_faster[] __initdata = {
+	{ 1, {   270000, HFPLL, 2, 0x0C }, L2(0),   850000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   875000 },
 	{ 0, {   432000, HFPLL, 2, 0x20 }, L2(6),   900000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),   900000 },
